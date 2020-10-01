cfe973c9b3650768c8a31157896bbd4898543b3b
Konstantin <you@example.com>
2020-10-01 08:53:39 +0300

    Make code refactoring

--- js/main.js
+++ js/main.js
@@ -37,6 +37,10 @@ const PIN = {
 const avatars = [];
 const DEFAULT_AVATAR = `img/avatars/default.png`;
 
+const map = document.querySelector(`.map`);
+const pinTemplate = document.querySelector(`#pin`).content;
+const pin = pinTemplate.querySelector(`.map__pin`);
+
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
@@ -60,18 +64,6 @@ const getAvatar = () => {
   return item;
 };
 
-const getType = () => {
-  return TYPE[getRandomInt(TYPE.length)];
-};
-
-const getCheckin = () => {
-  return CHECK_IN[getRandomInt(CHECK_IN.length)];
-};
-
-const getCheckout = () => {
-  return CHECK_OUT[getRandomInt(CHECK_OUT.length)];
-};
-
 const getFeatures = () => {
   const features = [];
   const repetition = [];
@@ -89,29 +81,31 @@ const getFeatures = () => {
 };
 
 const getAdvert = (x) => {
-  const advert = {};
-  const author = {};
-  const offer = {};
-  const location = {};
+  const xLocation = getRandomArbitrary(PIN.width / 2, x - PIN.width / 2) - PIN.width / 2;
+  const yLocation = getRandomArbitrary(LOCATION_Y_MIN + PIN.height, LOCATION_Y_MAX + PIN.height) - PIN.height;
 
-  author.avatar = getAvatar() || DEFAULT_AVATAR;
-  location.x = getRandomArbitrary(PIN.width / 2, x - PIN.width / 2) - PIN.width / 2;
-  location.y = getRandomArbitrary(LOCATION_Y_MIN + PIN.height, LOCATION_Y_MAX + PIN.height) - PIN.height;
-  offer.title = `заголовок предложения`;
-  offer.address = location.x + `, ` + location.y;
-  offer.price = getRandomArbitrary(ROOM_PRICE.min, ROOM_PRICE.max);
-  offer.type = getType();
-  offer.rooms = getRandomArbitrary(ROOM_COUNT.min, ROOM_COUNT.max);
-  offer.guests = getRandomArbitrary(GUESTS.min, GUESTS.max);
-  offer.checkin = getCheckin();
-  offer.checkout = getCheckout();
-  offer.features = getFeatures();
-  offer.description = `Описание`;
-  offer.photos = PHOTOS;
-
-  advert.autor = author;
-  advert.offer = offer;
-  advert.location = location;
+  const advert = {
+    "author": {
+      "avatar": getAvatar() || DEFAULT_AVATAR
+    },
+    "offer": {
+      "title": `заголовок предложения`,
+      "address": xLocation + `, ` + yLocation,
+      "price": getRandomArbitrary(ROOM_PRICE.min, ROOM_PRICE.max),
+      "type": TYPE[getRandomInt(TYPE.length)],
+      "rooms": getRandomArbitrary(ROOM_COUNT.min, ROOM_COUNT.max),
+      "guests": getRandomArbitrary(GUESTS.min, GUESTS.max),
+      "checkin": CHECK_IN[getRandomInt(CHECK_IN.length)],
+      "checkout": CHECK_OUT[getRandomInt(CHECK_OUT.length)],
+      "features": getFeatures(),
+      "description": `Описание`,
+      "photos": PHOTOS
+    },
+    "location": {
+      "x": xLocation,
+      "y": yLocation
+    }
+  };
 
   return advert;
 };
@@ -126,19 +120,16 @@ const getAdverts = (count, x) => {
 };
 
 const showMap = () => {
-  const map = document.querySelector(`.map`);
   map.classList.remove(`map--faded`);
-  return map;
 };
 
-const getPin = (advert, template) => {
-  const pin = template.querySelector(`.map__pin`);
+const getPin = (advert) => {
   const newPin = pin.cloneNode(true);
   const img = newPin.querySelector(`.map__pin-img`);
 
   newPin.style.left = advert.location.x + `px`;
   newPin.style.top = advert.location.y + `px`;
-  img.src = advert.autor.avatar;
+  img.src = advert.author.avatar;
   img.alt = advert.offer.title;
 
   return newPin;
@@ -146,23 +137,22 @@ const getPin = (advert, template) => {
 
 const getPins = (adverts) => {
   const pins = [];
-  const pinTemplate = document.querySelector(`#pin`).content;
   for (let i = 0; i < adverts.length; i++) {
-    pins.push(getPin(adverts[i], pinTemplate));
+    pins.push(getPin(adverts[i]));
   }
 
   return pins;
 };
 
-const showPins = (pins, map) => {
+const showPins = (pins) => {
   for (let i = 0; i < pins.length; i++) {
     map.appendChild(pins[i]);
   }
 };
 
 fillAvatars();
-const map = showMap();
+showMap();
 const adverts = getAdverts(ADVERT_COUNT, map.clientWidth);
 const pins = getPins(adverts);
-showPins(pins, map);
+showPins(pins);
 
